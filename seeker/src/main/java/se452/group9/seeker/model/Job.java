6e4d5f1749e5f9daf8e283c4b421bc80139a8dc2
Korey Lo <koreylo9@gmail.com>
2021-04-27 21:23:13 -0700

    added job class mapped to table

---
+++ seeker/src/main/java/se452/group9/seeker/model/Job.java
@@ -0,0 +1,77 @@
+package se452.group9.seeker.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Table(name = "jobs")
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title;
+    @Column(name="description", length=2000)
+    private String desc;
+    private String datePosted;
+    private Boolean isActive;
+    // private int companyID;
+   
+
+    //Constructor
+    // public Job(int id, String title, String description, Boolean isActive){
+    //     this.id = id;
+    //     this.title = title;
+    //     this.description = description;
+    //     this.createdDate = createdDate;
+    //     this.isActive = isActive;
+    // }
+    //Getters and Setters
+
+    public Long getID(){
+        return id;
+    }
+
+    public void setID(Long id){
+        this.id = id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDescription(){
+        return desc;
+    }
+
+    public void setDescription(String description){
+        this.desc = description;
+    }
+    public String getDatePosted(){
+        return datePosted;
+    }
+
+    public void setDatePosted(String datePosted){
+        this.datePosted = datePosted;
+    }
+
+    public Boolean getIsActive(){
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive){
+        this.isActive = isActive;
+    }
+
+}
