ca0f3404d76b0d370ba63b1f8310a0bf534958a8
Michael Arana <marana@depaul.edu>
2021-04-29 11:49:21 -0500

    Recruiter mapped.

--- seeker/src/main/java/se452/group9/seeker/Recruiter.java
+++ seeker/src/main/java/se452/group9/seeker/Recruiter.java
@@ -1,6 +1,11 @@
 package se452.group9.seeker;
 
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -8,23 +13,31 @@ import javax.validation.constraints.Size;
 
 import lombok.Data;
 
+@Entity
+@Table(name = "recruiters")
 @Data
 public class Recruiter {
 
+    @Id
+    @GeneratedValue
     @Min(value = 1, message = "recruiter ID must be greater than 0")
     @Max(value = 99999999, message = "recruiter ID can be at most 8 digits")
     private int recruiterID;
 
+    @Column(name = "companyID")
     @Min(value = 1, message = "company ID must be greater than 0")
     @Max(value = 99999999, message = "company ID can be at most 8 digits")
     private int companyID;
 
+    @Column(name = "fname")
     @Size(max = 50, message = "first name can be at most 50 chars")
     private String fName;
 
+    @Column(name = "lname")
     @Size(max = 50, message = "last name can be at most 50 chars")
     private String lName;
 
+    @Column(name = "email")
     @Email(message = "invalid email address format")
     private String email;
 
---
+++ seeker/src/main/java/se452/group9/seeker/RecruiterRepository.java
@@ -0,0 +1,7 @@
+package se452.group9.seeker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterRepository extends JpaRepository<Recruiter, Integer> {
+    
+}
