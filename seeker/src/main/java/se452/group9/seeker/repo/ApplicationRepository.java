b9095a11dd1c2e6fd0d2d653a53d8f7c16fe0b92
Korey Lo <koreylo9@gmail.com>
2021-04-30 00:10:20 -0700

    moved allans files into mvc folders

--- seeker/src/main/java/se452/group9/seeker/repo/ApplicationRepository.java
+++ seeker/src/main/java/se452/group9/seeker/repo/ApplicationRepository.java
@@ -1,9 +1,9 @@
-package se452.group9.seeker;
+package se452.group9.seeker.repo;
 
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import se452.group9.seeker.School;
+import se452.group9.seeker.model.School;
 
 public interface ApplicationRepository extends JpaRepository <Application, Long>{
 
--- seeker/src/main/java/se452/group9/seeker/repo/LocationRepository.java
+++ seeker/src/main/java/se452/group9/seeker/repo/LocationRepository.java
@@ -1,9 +1,9 @@
-package se452.group9.seeker;
+package se452.group9.seeker.model;
 
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import se452.group9.seeker.School;
+import se452.group9.seeker.model.School;
 
 public interface LocationRepository extends JpaRepository <Location, Long>{
 
