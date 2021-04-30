1e87141542a23120b60afc5c818842e001d1ea2e
agchang94 <82068031+agchang94@users.noreply.github.com>
2021-04-30 09:55:50 -0500

    Update LocationRepository.java
    fixed the package from se452.group9.seeker.model; -> se452.group9.seeker.repo;
    fixed the import from se452.group9.seeker.model.School; -> se452.group9.seeker.model.Location;

--- seeker/src/main/java/se452/group9/seeker/repo/LocationRepository.java
+++ seeker/src/main/java/se452/group9/seeker/repo/LocationRepository.java
@@ -1,9 +1,9 @@
-package se452.group9.seeker.model;
+package se452.group9.seeker.repo;
 
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import se452.group9.seeker.model.School;
+import se452.group9.seeker.model.Location;
 
 public interface LocationRepository extends JpaRepository <Location, Long>{
 
