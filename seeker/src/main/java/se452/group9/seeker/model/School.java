96bcfed83d0f090439da520d6b996eb6197e645f
Korey Lo <koreylo9@gmail.com>
2021-04-30 18:56:47 -0700

    fixed java validation error in shool.java

--- seeker/src/main/java/se452/group9/seeker/model/School.java
+++ seeker/src/main/java/se452/group9/seeker/model/School.java
@@ -3,7 +3,9 @@ package se452.group9.seeker.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 
 import lombok.Data;
 
@@ -12,7 +14,9 @@ import lombok.Data;
 @Table(name = "schools")
 public class School {
 
-    @Size(min = 1, max = 4, message = "school ID must be between 1 and 4 characters")
+    // @Size(min = 1, max = 4, message = "school ID must be between 1 and 4 characters")
+    @Max(value=4, message="school ID must be between 1 and 4 characters")
+    @Min(value=1, message="school ID must be between 1 and 4 characters")
     @Id
     private long schoolID;
     
