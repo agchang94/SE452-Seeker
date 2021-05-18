825f9dc7b9ac9183b6ae9bb0d5e7faeecd30d0d7
Korey Lo <koreylo9@gmail.com>
2021-05-18 11:46:26 -0700

    fixed some html in job listing

--- seeker/src/main/java/se452/group9/seeker/repo/JobRepository.java
+++ seeker/src/main/java/se452/group9/seeker/repo/JobRepository.java
@@ -4,7 +4,6 @@ import se452.group9.seeker.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 
 
