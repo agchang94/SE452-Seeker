6049af6963e3f6c0d65602d2138b2d57c9bba29b
Allan Chang <achang25@depaul.edu>
2021-05-30 16:57:56 -0500

    re-enabled TLS and added remember-me cookie for login

--- seeker/src/main/java/se452/group9/seeker/security/MainController.java
+++ seeker/src/main/java/se452/group9/seeker/security/MainController.java
@@ -1,13 +1,53 @@
 package se452.group9.seeker.security;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import se452.group9.seeker.model.Student;
+import se452.group9.seeker.repo.StudentRepository;
 
 @Controller
 public class MainController {
     
+    @Autowired
+    private DaoAuthenticationProvider test;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
     @GetMapping("/login")
     public String login() {
         return "login";
     }
+
+
+    /*@GetMapping("/studentProfile")
+    public String showProfile(HttpServletRequest request, HttpServletResponse response, Model model){
+
+        Cookie[] cookies = request.getCookies();
+
+        for(Cookie temp : cookies){
+            if("remember-me".equals(temp.getName())){
+                String userName = temp.getValue();
+                model.addAttribute("userName", userName);
+                return "studentProfile";
+            }
+        }
+
+        Cookie newCookie = new Cookie ("userName", "username");
+        newCookie.setMaxAge(60*60*24);
+        response.addCookie(newCookie);
+    
+        return "index";
+    }*/
 }
